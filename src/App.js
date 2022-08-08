aa89633b71d2a73a5e85be57113d190858cee284
user name <user emaile>
2022-08-08 15:55:12 +0300

    this is the authentication lab 15

--- src/App.js
+++ src/App.js
@@ -2,32 +2,44 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
+ import Profile from './Auth0/User';
 import 'bootstrap/dist/css/bootstrap.min.css';
+ 
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
-
 class App extends React.Component {
   render() {
     return (
       <>
         <Router>
           <Header />
+          
           <Routes>
             <Route 
               exact path="/"
               element={<BestBooks />}
             >
-            </Route>
+
+            <Route
+            exact path="/Profile"
+            element={<Profile />} 
+            ></Route>
+
+          </Route>
+             
             {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
           </Routes>
+          
           <Footer />
         </Router>
       </>
     )
   }
 }
+
+
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import AuthButton from './Auth0/AuthButtons';
+import User from './Auth0/User';
 
 class Header extends React.Component {
   render() {
@@ -9,6 +11,8 @@ class Header extends React.Component {
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
         {/* PLACEHOLDER: render a navigation link to the about page */}
+        <AuthButton />
+        <User />
       </Navbar>
     )
   }
